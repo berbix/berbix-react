08dc861bb03b61c0920e8c01e09a92e6bc8afb42
Eric Levine <eric@berbix.com>
2019-08-01 15:00:54 -0700

    Updating Berbix React SDK version to 0.0.5

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
 
-const SDK_VERSION = '0.0.4';
+const SDK_VERSION = '0.0.5';
 
 class BerbixVerify extends React.Component {
   constructor(props) {
